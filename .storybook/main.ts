b6859bfe4df93e8181d7ab0ba08c5d7fd4676354
홍규진 <hkj0206@dgu.ac.kr>
2025-03-10 21:09:05 +0900

    feat: storybook 설정 파일

---
+++ .storybook/main.ts
@@ -0,0 +1,8 @@
+import {StorybookConfig} from '@storybook/react-native';
+
+const main: StorybookConfig = {
+  stories: ['./.storybook/stories/**/*.stories.tsx'],
+  addons: [],
+};
+
+export default main;
