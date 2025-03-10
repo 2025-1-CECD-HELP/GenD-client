3a1ad747d201825aa65e0e78f1a8e190a397d897
홍규진 <hkj0206@dgu.ac.kr>
2025-03-10 21:08:24 +0900

    feat: storybook Metro 번들러 설정 모듈화

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,43 @@
+const path = require('path');
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const withStorybook = require('@storybook/react-native/metro/withStorybook');
 
 /**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- * @type {import('@react-native/metro-config').MetroConfig}
+ * Metro 설정을 위한 Default 설정들만 따로 모듈화 합니다.
+ * @author 홍규진
  */
-const config = {
+const defaultConfig = getDefaultConfig(__dirname);
+
+/**
+ * 프로젝트 설정을 위한 설정들만 따로 모듈화 합니다.
+ * @author 홍규진
+ */
+const projectConfig = {
+  transformer: {
+    unstable_allowRequireContext: true, // require.context 지원 활성화 (Storybook에서 필요)
+  },
   resolver: {
-    resolverMainFields: ['sbmodern', 'react-native', 'browser', 'main'],
-    sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx'],
+    resolverMainFields: ['sbmodern', 'react-native', 'browser', 'main'], // Storybook 우선 로드
+    sourceExts: [...defaultConfig.resolver.sourceExts, 'cjs', 'mjs'], // 추가 확장자 지원
+    assetExts: [...defaultConfig.resolver.assetExts], // 기본 자산 확장자 유지
     alias: {
-      '@': './src',
+      '@': path.resolve(__dirname, './src'), // @ 경로 별칭 추가 (src 폴더)
     },
   },
 };
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const finalConfig = mergeConfig(defaultConfig, projectConfig);
+
+/**
+ * Storybook 설정을 위한 설정들만 따로 모듈화 합니다.
+ * @author 홍규진
+ */
+const storybookConfig = {
+  enabled: true,
+  // Path to your storybook config
+  configPath: path.resolve(__dirname, './.storybook'),
+};
+
+module.exports = (async () => {
+  return withStorybook(finalConfig, storybookConfig);
+})();
