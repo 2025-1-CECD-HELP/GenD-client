71cf9ba2ca3218e3edfa3e53c145603980b5473a
홍규진 <120018983+Kyujenius@users.noreply.github.com>
2025-04-06 15:46:02 +0900

    Revert "Feature/#30 - ChattingInput 컴포넌트 구현"

--- src/assets/images/svg/bottom-navigator/index.ts
+++ src/assets/images/svg/bottom-navigator/index.ts
@@ -2,3 +2,4 @@ export {default as HomeIcon} from './home.svg';
 export {default as FileIcon} from './file.svg';
 export {default as CalendarIcon} from './calendar.svg';
 export {default as MypageIcon} from './my-page.svg';
+export {default as LogoIcon} from './logo.svg';
