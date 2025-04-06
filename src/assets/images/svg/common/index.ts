ead18bf76777df1b644418a43788ca546bd97719
홍규진 <hkj0206@dgu.ac.kr>
2025-04-06 16:55:19 +0900

    feat: 아이콘 추가

--- src/assets/images/svg/common/index.ts
+++ src/assets/images/svg/common/index.ts
@@ -1,2 +1,3 @@
 export {default as SettingIcon} from './setting.svg';
 export {default as CancelIcon} from './cancel.svg';
+export {default as PlusIcon} from './plus.svg';
---
+++ src/assets/images/svg/search-bar/index.ts
@@ -0,0 +1,2 @@
+export {default as ExportIcon} from './export.svg';
+export {default as SearchIcon} from './search.svg';
