4f604b72980fba575537b01580153fe75e9a3859
jungsun <dlwjdtjs0111@gmail.com>
2025-04-08 17:54:44 +0900

    fix: 주석 추가

--- src/hooks/useFilePreviewList.ts
+++ src/hooks/useFilePreviewList.ts
@@ -1,6 +1,16 @@
 import {useCallback} from 'react';
 import {useResponsiveGrid} from './useResponsiveGrid';
 import {FileData} from '@/components/FilePreview/index.type';
+/**
+ * 해당 훅은 FilePreviewList 컴포넌트의 로직을 분리하여 관리합니다.
+ * 사용자 권한에 따라 액션 버튼 클릭 시 실행될 함수(handlePressAction)를 각 권한에 맞게 반환합니다.
+ * 또한 파일 클릭 시 호출되는 handlePressFile 콜백을 제공합니다.
+ * useResponsiveGrid 훅을 활용하여 그리드 레이아웃을 구성하는 데 필요한 값을 반환합니다.
+ *
+ * 이를 통해 FilePreviewList 컴포넌트 내에서는 UI 관련 로직만 관리하고,
+ * 각종 핸들링 및 레이아웃 계산 로직은 본 훅에서 한번에 처리할 수 있습니다.
+ * @author 이정선
+ */
 
 export const useFilePreviewList = (
   position: 'member' | 'manager',
