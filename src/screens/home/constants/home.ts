044b2174ee05e370437fc72b0402b2215af12f93
jungsun <dlwjdtjs0111@gmail.com>
2025-05-05 20:32:05 +0900

    fix: 더미데이터 수정

--- src/screens/home/constants/home.ts
+++ src/screens/home/constants/home.ts
@@ -28,7 +28,7 @@ export const DUMMY_POST = [
     title: '게시글의 제목',
     description:
       '이번 주 워크샵은 금요일 오후 2시에 진행될 예정입니다. 장소는 5층 대회의실이며, 발표 자료와 아이디어 공유를 중심으로 구성됩니다. 참석자분들을 위해 간단한 다과도 준비할 예정이니 많은 참여 부탁드립니다!',
-    writer: '내꺼아님',
+    writer: '홍규진',
     imageUrl:
       'https://img.freepik.com/free-photo/group-casually-dressed-business-people-discussing-ideas-office_7861-1634.jpg?t=st=1743401160~exp=1743404760~hmac=70352676a3f4a5124aa04f3fe77d9a7d2184b9f92b06c23148647b808a4959d8&w=2000',
     isPin: false,
@@ -39,7 +39,7 @@ export const DUMMY_POST = [
     title: '게시글의 제목',
     description:
       '이번 주 워크샵은 금요일 오후 2시에 진행될 예정입니다. 장소는 5층 대회의실이며, 발표 자료와 아이디어 공유를 중심으로 구성됩니다. 참석자분들을 위해 간단한 다과도 준비할 예정이니 많은 참여 부탁드립니다!',
-    writer: '내꺼아님',
+    writer: '박민기',
     imageUrl:
       'https://img.freepik.com/free-photo/group-casually-dressed-business-people-discussing-ideas-office_7861-1634.jpg?t=st=1743401160~exp=1743404760~hmac=70352676a3f4a5124aa04f3fe77d9a7d2184b9f92b06c23148647b808a4959d8&w=2000',
     isPin: false,
@@ -50,7 +50,7 @@ export const DUMMY_POST = [
     title: '게시글의 제목',
     description:
       '이번 주 워크샵은 금요일 오후 2시에 진행될 예정입니다. 장소는 5층 대회의실이며, 발표 자료와 아이디어 공유를 중심으로 구성됩니다. 참석자분들을 위해 간단한 다과도 준비할 예정이니 많은 참여 부탁드립니다!',
-    writer: '내꺼아님',
+    writer: '이정선',
     imageUrl:
       'https://img.freepik.com/free-photo/group-casually-dressed-business-people-discussing-ideas-office_7861-1634.jpg?t=st=1743401160~exp=1743404760~hmac=70352676a3f4a5124aa04f3fe77d9a7d2184b9f92b06c23148647b808a4959d8&w=2000',
     isPin: false,
@@ -61,7 +61,7 @@ export const DUMMY_POST = [
     title: '게시글의 제목',
     description:
       '이번 주 워크샵은 금요일 오후 2시에 진행될 예정입니다. 장소는 5층 대회의실이며, 발표 자료와 아이디어 공유를 중심으로 구성됩니다. 참석자분들을 위해 간단한 다과도 준비할 예정이니 많은 참여 부탁드립니다!',
-    writer: '내꺼아님',
+    writer: '엄태우',
     imageUrl:
       'https://img.freepik.com/free-photo/group-casually-dressed-business-people-discussing-ideas-office_7861-1634.jpg?t=st=1743401160~exp=1743404760~hmac=70352676a3f4a5124aa04f3fe77d9a7d2184b9f92b06c23148647b808a4959d8&w=2000',
     isPin: false,
@@ -73,6 +73,6 @@ export const DUMMY_WORKSPACE_INFO = {
   workspaceName: 'GenD',
   workspaceDescription: '단체를 위한 맞춤형 워크스페이스를 제공합니다.',
   imageUrl:
-    'https://hiviewsolutions.com/wp-content/uploads/2024/08/HiView-Solutions-Google-Workspace-Reseller.-Super-G-Icon-e1725156398786.png',
+    'https://images.unsplash.com/photo-1557682250-33bd709cbe85?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8JUVCJUIzJUI0JUVCJTlEJUJDJUVDJTgzJTg5JTIwJUVCJUIwJUIwJUVBJUIyJUJEfGVufDB8fDB8fHww',
   workspaceRole: 'eAdmin',
 };
