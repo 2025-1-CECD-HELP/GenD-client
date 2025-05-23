487bffa0b4e452ec6bfeaa0b7715d41e8bd2d7e5
홍규진 <hkj0206@dgu.ac.kr>
2025-05-23 12:48:35 +0900

    feat: recording 내 컴포넌트 구현

---
+++ src/screens/recording/components/RecordingSubmitModal/index.style.ts
@@ -0,0 +1,33 @@
+import styled from '@emotion/native';
+
+export const SubmitFormContainer = styled.View`
+  width: 100%;
+`;
+
+export const SubmitInput = styled.TextInput`
+  border-bottom-width: 1px;
+  border-bottom-color: ${({theme}) => theme.colors.divider};
+  font-size: 16px;
+  color: ${({theme}) => theme.colors.textPrimary};
+  margin-bottom: 18px;
+  padding: 6px 0;
+`;
+
+export const SubmitRow = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 12px;
+`;
+
+export const SubmitLabel = styled.Text`
+  flex: 1;
+  color: ${({theme}) => theme.colors.textSecondary};
+  font-size: 15px;
+`;
+
+export const SubmitDropdown = styled.TouchableOpacity`
+  flex: 2;
+  border-bottom-width: 1px;
+  border-bottom-color: ${({theme}) => theme.colors.divider};
+  padding: 6px 0;
+`;
