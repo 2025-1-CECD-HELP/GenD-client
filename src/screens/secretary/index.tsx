d0116b42e0409fe0682dd214295498eb8f47f22b
홍규진 <hkj0206@dgu.ac.kr>
2025-05-25 14:13:20 +0900

    feat: 비서 채팅 Screen 및 Util 함수 구현

---
+++ src/screens/secretary/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Container} from './index.style';
+
+import {TopBar} from '@/components/TopBar';
+import ChattingInput from '@/components/ChattingInput';
+import {SecretaryProfile} from './components/ScretaryProfile';
+import {SecretaryChatList} from './components/SecretaryChatList';
+import {useChat} from './contexts/ChatContext';
+import {useThemeColors} from '@/contexts/theme/ThemeContext';
+
+/**
+ * 비서 화면입니다.
+ * 비서 프로필, 채팅 리스트, 채팅 입력창으로 구성됩니다.
+ * @author 홍규진
+ */
+const SecretaryScreen = () => {
+  const {sendChatting} = useChat();
+  const {textPrimary} = useThemeColors();
+  return (
+    <Container>
+      <TopBar title="AI 비서" />
+      <SecretaryProfile />
+      <SecretaryChatList />
+      <ChattingInput
+        onSubmit={chattingText => {
+          sendChatting({
+            secretaryQuestion: chattingText,
+          });
+        }}
+        placeholder="AI 비서에게 질문해보세요"
+        placeholderTextColor={textPrimary}
+      />
+    </Container>
+  );
+};
+
+export default SecretaryScreen;
