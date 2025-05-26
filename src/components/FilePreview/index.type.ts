5732b0b0f177b89b9b1f86438d2ec647a12d199b
홍규진 <hkj0206@dgu.ac.kr>
2025-05-27 02:07:42 +0900

    feat: 파일 화면 컴포넌트 리팩토링

--- src/components/FilePreview/index.type.ts
+++ src/components/FilePreview/index.type.ts
@@ -1,5 +1,7 @@
 export type FileData = {
-  title: string;
-  extension: 'mp3' | 'docx'; // 파일 확장자 (지원 형식 제한)
-  url: string; // 파일 url
+  documentId: number;
+  documentTitle: string;
+  docuementType: string;
+  createdAt: string;
+  documentFile: string;
 };
--- src/components/FolderPreview/index.style.ts
+++ src/components/FolderPreview/index.style.ts
@@ -1,6 +1,6 @@
 import styled from '@emotion/native';
 
-export const Container = styled.View`
+export const Container = styled.TouchableOpacity`
   flex: 1;
   border-radius: 13px;
   border: 1px solid ${({theme}) => theme.colors.divider};
@@ -33,4 +33,29 @@ export const ContentContainer = styled.View`
 export const Title = styled.Text`
   ${({theme}) => theme.fonts.title5};
   color: ${({theme}) => theme.colors.textPrimary};
+`;
+
+export const Card = styled.View`
+  background: #f7f8fa;
+  border-radius: 16px;
+  align-items: center;
+  margin-bottom: 20px;
+  padding: 18px 0 10px 0;
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.06);
+`;
+
+export const IconWrapper = styled.View`
+  margin-bottom: 8px;
+`;
+
+export const FolderName = styled.Text`
+  font-size: 16px;
+  font-weight: 600;
+  color: ${({theme}) => theme.colors.textPrimary};
+`;
+
+export const ItemCount = styled.Text`
+  font-size: 12px;
+  color: ${({theme}) => theme.colors.textDisabled};
+  margin-top: 2px;
 `;
