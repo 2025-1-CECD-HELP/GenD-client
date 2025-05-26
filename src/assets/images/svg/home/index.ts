e0a64385d414a32b27a12758d767350f08b68529
홍규진 <hkj0206@dgu.ac.kr>
2025-05-27 02:32:12 +0900

    feat: assets 추가

--- src/assets/images/svg/home/index.ts
+++ src/assets/images/svg/home/index.ts
@@ -3,3 +3,4 @@ export {default as MemberIcon} from './member.svg';
 export {default as MoreIcon} from './more.svg';
 export {default as WriteIcon} from './write.svg';
 export {default as PlusIcon} from './plus.svg';
+export {default as ChattingIcon} from './chatting.svg';
