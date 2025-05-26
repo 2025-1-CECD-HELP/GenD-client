15c3525995216a0bb616d432209e025b4ece8187
홍규진 <hkj0206@dgu.ac.kr>
2025-05-27 02:36:07 +0900

    feat: 워크스페이스 생성 custom-hook 구현 및 Mutation 적용

--- src/screens/create-workspace/hooks/useCreateWorkspaceMutation.tsx
+++ src/screens/create-workspace/hooks/useCreateWorkspaceMutation.tsx
@@ -1,16 +1,22 @@
 import {useMutation, useQueryClient} from '@tanstack/react-query';
 import {TCreateWorkspaceRequest} from '@/services/workspace/types';
 import {createWorkspaceMutationKey} from '@/constants/mutationKeys';
+import useTypeSafeNavigation from '@/hooks/useTypeSafeNavigaion';
+import {useModal} from '@/contexts/modal/ModalContext';
+import CommonModal from '@/components/CommonModal';
 
 /**
  * 워크스페이스 생성 뮤테이션 훅입니다.
  * 워크스페이스 생성 요청을 처리합니다.
  * 워크스페이스 생성시에 워크스페이스 목록 조회 캐시를 무효화합니다.
+ * 성공시엔 성공 모달을 띄우고, 확인을 누를 시에 워크스페이스 생성 화면으로 이동합니다.
  * @returns 워크스페이스 생성 뮤테이션 함수
  * @author 홍규진
  */
 export const useCreateWorkspaceMutation = () => {
   const queryClient = useQueryClient();
+  const navigation = useTypeSafeNavigation();
+  const {setIsOpen, setModalContent} = useModal();
   const {mutate} = useMutation({
     mutationKey: createWorkspaceMutationKey().mutationKey,
     mutationFn: (workspace: TCreateWorkspaceRequest) =>
@@ -19,6 +25,17 @@ export const useCreateWorkspaceMutation = () => {
       queryClient.invalidateQueries({
         queryKey: createWorkspaceMutationKey().mutationSuccessKey,
       });
+      setIsOpen(true);
+      setModalContent(
+        <CommonModal
+          type={'check'}
+          title={'워크스페이스 생성 완료'}
+          isCenter={true}
+          onConfirm={() => {
+            navigation.navigate('INIT_WORKSPACE', {});
+          }}
+        />,
+      );
     },
   });
 
