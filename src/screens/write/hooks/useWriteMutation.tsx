1f3e1a055ce71869a95b2c870a6f729e06b53f01
홍규진 <hkj0206@dgu.ac.kr>
2025-05-27 02:48:57 +0900

    feat: 글쓰기 화면 Mutation 구현 및 custom-hook을 통한 적용

---
+++ src/screens/write/hooks/useWriteMutation.tsx
@@ -0,0 +1,73 @@
+import useTypeSafeNavigation from '@/hooks/useTypeSafeNavigaion';
+import {createPost} from '@/services/post';
+import {TPostWriteRequest} from '@/services/post/types';
+import {Asset} from 'react-native-image-picker';
+import {useMutation, useQueryClient} from '@tanstack/react-query';
+import {useModal} from '@contexts/modal/ModalContext';
+import CommonModal from '@components/CommonModal';
+import {useWorkspace} from '@hooks/useWorkspace';
+
+type TWriteMutationVariables = {
+  request: TPostWriteRequest;
+  imageFile: Asset;
+};
+
+/**
+ * 게시글 작성 뮤테이션 훅입니다.
+ * 게시글 작성 요청을 처리합니다.
+ * @returns {object} mutate, isLoading, isError 상태 반환
+ * @author 홍규진
+ */
+export const useWriteMutation = () => {
+  const queryClient = useQueryClient();
+  const navigation = useTypeSafeNavigation();
+  const {setIsOpen, setModalContent} = useModal();
+  const {workspaceId} = useWorkspace();
+
+  const mutation = useMutation({
+    mutationKey: ['write'],
+    mutationFn: ({request, imageFile}: TWriteMutationVariables) => {
+      return createPost(workspaceId!, request, imageFile);
+    },
+
+    onSuccess: async () => {
+      showPostSuccessModal();
+      queryClient.invalidateQueries({queryKey: ['post']});
+    },
+    onError: () => {
+      showErrorModal();
+    },
+  });
+
+  const showPostSuccessModal = () => {
+    setIsOpen(true);
+    setModalContent(
+      <CommonModal
+        type="check"
+        title="게시글이 등록되었습니다!"
+        content="다른 사용자들과 지금 공유해보세요."
+        isCenter={true}
+        onConfirm={() => {
+          setIsOpen(false);
+          navigation.navigate('LANDING', {});
+        }}
+      />,
+    );
+  };
+
+  const showErrorModal = () => {
+    setIsOpen(true);
+    setModalContent(
+      <CommonModal
+        type="check"
+        title="게시글 등록 실패"
+        content="게시글 등록에 실패했습니다. 다시 시도해주세요."
+        isCenter={true}
+        onConfirm={() => {
+          setIsOpen(false);
+        }}
+      />,
+    );
+  };
+  return mutation;
+};
